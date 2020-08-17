a4b45d3ee88f5880ee401f9740a1739b20b3e122
vism2889 <morganticss@gmail.com>
2020-08-17 18:04:20 -0400

    directory of videos instead of single video

--- Object_Detection/video_to_images.py
+++ Object_Detection/video_to_images.py
@@ -7,6 +7,11 @@
 # INFO:
 # - Script to convert video to images as a means for gathering training data for object detection
 # - uses detecto library
+#
+# TODO:
+# - Modify to take directory of videos and output directory of frames per video.
+#
+#
 
 
 from detecto.utils import split_video
@@ -14,16 +19,29 @@ import os
 
 
 
-vidPath = '/home/morgan/Downloads/testVid.mp4'
-frameOutputPath = '/home/morgan/Downloads/testVidFrames2/'
-frameSkip = 10
+# path to directory with all videos in it
+path_to_video_folder = '/home/morgan/Videos_data/' 
+
+# path to directory in which you wish to output video frame folders to
+output_from_video_dir = '/home/morgan/video_to_image_exports/'
 
 
-# checks to see if output directory exists, if not it is created
-if not os.path.exists(frameOutputPath):
-    os.mkdir(frameOutputPath)
+video_list = os.listdir(path_to_video_folder)
+frame_skip = 10
 
+# creates output folder if doesn't exist
+if not os.path.exists(output_from_video_dir):
+    os.mkdir(output_from_video_dir)
 
+for i in range(len(video_list)):
+    
+    print('generating images for: ' + video_list[i])
+    vid_path = path_to_video_folder + video_list[i]
+    frame_output_path = output_from_video_dir + "video_to_image_output_" + str(i)
+    
+    # creates output folder if doesn't exist
+    if not os.path.exists(frame_output_path):
+        os.mkdir(frame_output_path)
+    
+    split_video(vid_path, frame_output_path, step_size=frame_skip)
 
-#split_video('video file path', 'image save path', frame size)
-split_video(vidPath, frameOutputPath, step_size=frameSkip)
