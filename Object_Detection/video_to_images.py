caa5620bff68a01c39ef3053c06d0b5447353bae
vism2889 <morganticss@gmail.com>
2020-08-18 14:25:03 -0400

    update

--- Object_Detection/video_to_images.py
+++ Object_Detection/video_to_images.py
@@ -9,7 +9,6 @@
 # - uses detecto library
 #
 # TODO:
-# - Modify to take directory of videos and output directory of frames per video.
 #
 #
 
@@ -17,7 +16,10 @@
 from detecto.utils import split_video
 import os
 
-
+def generateNewFilename(oldName):
+    folderName = oldName[:-4]
+    name = folderName.replace(" ","_")
+    return name 
 
 # path to directory with all videos in it
 path_to_video_folder = '/home/morgan/Videos_data/' 
@@ -28,20 +30,21 @@ output_from_video_dir = '/home/morgan/video_to_image_exports/'
 
 video_list = os.listdir(path_to_video_folder)
 frame_skip = 10
+output_prefix = 'racecar_'
 
 # creates output folder if doesn't exist
 if not os.path.exists(output_from_video_dir):
     os.mkdir(output_from_video_dir)
 
 for i in range(len(video_list)):
-    
-    print('generating images for: ' + video_list[i])
+    name = generateNewFilename(video_list[i])
+    print('generating images for: ' + name)
     vid_path = path_to_video_folder + video_list[i]
-    frame_output_path = output_from_video_dir + "video_to_image_output_" + str(i)
+    frame_output_path = output_from_video_dir + "frame_output_" + name
     
     # creates output folder if doesn't exist
     if not os.path.exists(frame_output_path):
         os.mkdir(frame_output_path)
     
-    split_video(vid_path, frame_output_path, step_size=frame_skip)
+    split_video(vid_path, frame_output_path, prefix=output_prefix, step_size=frame_skip)
 
